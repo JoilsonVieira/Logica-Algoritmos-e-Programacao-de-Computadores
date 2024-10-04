c963e006f23d90c3acc438a4b8c4f34836bd6a1a
JoilsonVieira <joilsontil@gmail.com>
2024-10-03 21:50:53 -0300

    - Atividades 1, 2 e 3.

---
+++ estruturas_condicao1.py
@@ -0,0 +1,12 @@
+#Crie uma variável chamada temperatura e atribua a ela o valor 29;
+#Crie uma verificação, utilizando a condição if, para checar se o valor da variável temperatura é menor que 30;
+#Caso positivo, imprima na tela o texto ‘A temperatura hoje está amena’;
+#Caso contrário, e utilizando a condição else, imprima na tela o texto ‘Hoje está fazendo calor’;
+
+TEMPERATURA = 31
+
+if (TEMPERATURA < 30):
+  print ('A temperatura hoje está amena')
+  
+else:
+  print ('Hoje está fazendo calor')
