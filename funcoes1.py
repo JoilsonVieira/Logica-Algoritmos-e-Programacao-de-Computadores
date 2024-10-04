22735710da679140c9cad4022baaf8535feaacae
JoilsonVieira <joilsontil@gmail.com>
2024-10-03 23:11:43 -0300

    funcoes

---
+++ funcoes1.py
@@ -0,0 +1,13 @@
+# Defina uma função chamada “imprimir_variavel”. Tal instrução não
+# receberá parâmetros;
+# No escopo da função acima, crie uma variável chamada texto e atribua a
+# ela o valor ‘Olá, funções em Python’;
+# A seguir, ainda no escopo da função, imprima na tela o valor da variável
+# texto;
+# Por último, fora do escopo da função, faça a chamada da mesma.
+
+def imprimir_variavel():
+    texto = 'Olá, funções em Python'
+    print (texto)
+
+imprimir_variavel()
